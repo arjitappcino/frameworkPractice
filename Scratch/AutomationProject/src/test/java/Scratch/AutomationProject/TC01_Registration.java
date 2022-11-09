f026392d41bc52d78d6316a39f74e8729e6b37f4
arjitappcino <arjit.yadav@appcino.com>
2022-11-09 11:17:48 +0530

    Deleted userdata file

--- Scratch/AutomationProject/src/test/java/Scratch/AutomationProject/TC01_Registration.java
+++ Scratch/AutomationProject/src/test/java/Scratch/AutomationProject/TC01_Registration.java
@@ -21,6 +21,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageFactory.RegisterElements;
+import utility.dataFetch;
 import utility.utils;
 
 public class TC01_Registration {
@@ -33,10 +34,9 @@ public class TC01_Registration {
 	public static XSSFWorkbook workbook;
 	public static XSSFSheet worksheet;
 	public static DataFormatter formatter = new DataFormatter();
-	//public static String file_location = System.getProperty("user.dir") + "/Akeneo_product";
+	// public static String file_location = System.getProperty("user.dir") +
+	// "/Akeneo_product";
 	static String SheetName = "Sheet1";
-	public String Res;
-	public int DataSet = -1;
 
 	@BeforeMethod
 	public void beforeMethod() throws IOException {
@@ -93,38 +93,9 @@ public class TC01_Registration {
 	@DataProvider
 	public Object[][] registerData() throws Exception {
 
-		FileInputStream fileInputStream = new FileInputStream(file_location); // Excel sheet file location get mentioned
-																				// here
-		workbook = new XSSFWorkbook(fileInputStream); // get my workbook
-		worksheet = workbook.getSheet(SheetName);// get my sheet from workbook
-		XSSFRow Row = worksheet.getRow(0); // get my Row which start from 0
+		Object[][] testObjArray = dataFetch.readExcel(file_location, SheetName);
 
-		int RowNum = worksheet.getPhysicalNumberOfRows();// count my number of Rows
-		int ColNum = Row.getLastCellNum(); // get last ColNum
+		return (testObjArray);
 
-		Object Data[][] = new Object[RowNum - 1][ColNum]; // pass my count data in array
-
-		for (int i = 0; i < RowNum - 1; i++) // Loop work for Rows
-		{
-			XSSFRow row = worksheet.getRow(i + 1);
-
-			for (int j = 0; j < ColNum; j++) // Loop work for colNum
-			{
-				if (row == null)
-					Data[i][j] = "";
-				else {
-					XSSFCell cell = row.getCell(j);
-					if (cell == null)
-						Data[i][j] = ""; // if it get Null value it pass no data
-					else {
-						String value = formatter.formatCellValue(cell);
-						Data[i][j] = value; // This formatter get my all values as string i.e integer, float all type
-											// data value
-					}
-				}
-			}
-		}
-
-		return Data;
 	}
 }
--- Scratch/AutomationProject/src/test/java/utility/dataFetch.java
+++ Scratch/AutomationProject/src/test/java/utility/dataFetch.java
@@ -1,40 +1,56 @@
+package utility;
+
 import java.io.FileInputStream;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class dataFetch {
 
-	FileInputStream fileInputStream = new FileInputStream("D:\\GIT Automation Practice\\frameworkPractice\\Scratch\\AutomationProject\\UserData.xlsx"); // Excel sheet file location get mentioned
-															// here
-	workbook=new HSSFWorkbook(fileInputStream); // get my workbook
-	worksheet=workbook.getSheet(SheetName);// get my sheet from workbook
-	HSSFRow Row = worksheet.getRow(0); // get my Row which start from 0
+	private static XSSFSheet worksheet;
+	private static XSSFWorkbook workbook;
+	public static DataFormatter formatter = new DataFormatter();
 
-	int RowNum = worksheet.getPhysicalNumberOfRows();// count my number of Rows
-	int ColNum = Row.getLastCellNum(); // get last ColNum
+	public static Object[][] readExcel(String Path, String SheetName) throws Exception {
 
-	Object Data[][] = new Object[RowNum - 1][ColNum]; // pass my count data in array
+		FileInputStream fileInputStream = new FileInputStream(Path); // Excel sheet file location get mentioned
+		// here
+		workbook = new XSSFWorkbook(fileInputStream); // get my workbook
+		worksheet = workbook.getSheet(SheetName);// get my sheet from workbook
+		XSSFRow Row = worksheet.getRow(0); // get my Row which start from 0
 
-	for(
-	int i = 0;i<RowNum-1;i++) // Loop work for Rows
-	{
-		HSSFRow row = worksheet.getRow(i + 1);
+		int RowNum = worksheet.getPhysicalNumberOfRows();// count my number of Rows
+		int ColNum = Row.getLastCellNum(); // get last ColNum
 
-		for (int j = 0; j < ColNum; j++) // Loop work for colNum
+		Object Data[][] = new Object[RowNum - 1][ColNum]; // pass my count data in array
+
+		for (int i = 0; i < RowNum - 1; i++) // Loop work for Rows
 		{
-			if (row == null)
-				Data[i][j] = "";
-			else {
-				HSSFCell cell = row.getCell(j);
-				if (cell == null)
-					Data[i][j] = ""; // if it get Null value it pass no data
+			XSSFRow row = worksheet.getRow(i + 1);
+
+			for (int j = 0; j < ColNum; j++) // Loop work for colNum
+			{
+				if (row == null)
+					Data[i][j] = "";
 				else {
-					String value = formatter.formatCellValue(cell);
-					Data[i][j] = value; // This formatter get my all values as string i.e integer, float all type
-										// data value
+					XSSFCell cell = row.getCell(j);
+					if (cell == null)
+						Data[i][j] = ""; // if it get Null value it pass no data
+					else {
+						// This formatter get my all values as string i.e integer, float all type
+						String value = formatter.formatCellValue(cell);
+						// data value
+						Data[i][j] = value;
+
+					}
 				}
 			}
 		}
+		return Data;
+
 	}
+
 }
